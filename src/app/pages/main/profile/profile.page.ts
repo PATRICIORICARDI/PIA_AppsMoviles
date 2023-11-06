445ef3eb5bb3cff7d847e4f3062a7bf6d53e8c58
Nialeksan1 <100547287+Nialeksan1@users.noreply.github.com>
2023-11-05 20:05:00 -0600

    Pia_Incompleto
    Solo faltan sus partes

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Usuario } from 'src/app/models/user.model';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  utilsService = inject(UtilsService);
+  firebaseService = inject(FirebaseService);
 
   ngOnInit() {
+  }
+
+  user(): Usuario {
+    return this.utilsService.getLocalStorage('user');
+  }
+
+  async takeImage() {
+
+    let user = this.user();
+
+    let path = `users/${user.uid}`;
+
+    const dataUrl = (await this.utilsService.takePicture('Imagen del perfil')).dataUrl; // Extraer la respuesta
+    const loading = await this.utilsService.loading();
+    await loading.present();
+
+    let imgPath = `${user.uid}/profile`;
+    user.img = await this.firebaseService.updateImg(imgPath, dataUrl);
+    this.firebaseService.updateDocument(path, { img: user.img })
+      .then(async resp => {
+        this.utilsService.saveLocalStorage('user', user);
+
+        this.utilsService.presentToast({
+          message: `Imagen actualizada exitósamente`,
+          duration: 1500,
+          color: 'primary',
+          position: 'top',
+          icon: 'checkmark-circle-outline'
+        })
+        
+      }).catch(error => {
+        console.log(error);
+        this.utilsService.presentToast({
+          message: error.message,
+          duration: 2500,
+          color: 'danger',
+          position: 'top',
+          icon: 'alert-circle-outline'
+        })
+      }).finally(() => {
+        loading.dismiss();
+      })
   }
 
 }
